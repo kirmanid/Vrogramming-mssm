8475139a13a0317e38bdeacc408af4996caf3c2b
Matt <virginm@mssm.org>
2019-11-08 18:52:17 -0500

    intake/outtake works, but everything that needed a negative value now needs positive and vice versa because of the addition of rotation units to the motors. Added some work to autonomous, but pressing A on the controller does not activate autonomous.

--- src/main.cpp
+++ src/main.cpp
@@ -10,34 +10,36 @@ struct controllerState {
 	bool rightBumper2;
 	bool leftBumper2;
 	bool grabblerFresh;
+	bool a;
 } controls;
 
 Controller master (E_CONTROLLER_MASTER);
 
-Motor frontLeftMotor(11);
-Motor frontRightMotor(17);
-Motor backLeftMotor(19);
-Motor backRightMotor(18);
-Motor leftGrabblerMotor(14);
-Motor rightGrabblerMotor(16);
-Motor retractRamp(15);
+Motor frontLeftMotor(11, E_MOTOR_ENCODER_ROTATIONS); //rotations = distance/circumference -- circumference = 12.5663706144 inches (maybe use 4.15 if this doesn't work)
+Motor frontRightMotor(17, E_MOTOR_ENCODER_ROTATIONS);
+Motor backLeftMotor(19, E_MOTOR_ENCODER_ROTATIONS);
+Motor backRightMotor(18, E_MOTOR_ENCODER_ROTATIONS);
+Motor leftGrabblerMotor(14, E_MOTOR_ENCODER_ROTATIONS); //maybe remove rotations if it doesnt work
+Motor rightGrabblerMotor(16, E_MOTOR_ENCODER_ROTATIONS);
+Motor retractRamp(15, E_MOTOR_ENCODER_ROTATIONS);
 
 controllerState getControllerState (){
 	controls.left = master.get_analog(ANALOG_LEFT_Y); //y val of left stick
 	controls.right = master.get_analog(ANALOG_RIGHT_Y); // y val of right stick
-	
+
 	controls.rightBumper1 = master.get_digital(DIGITAL_R1);
-	controls.leftBumper1 = master.get_digital(DIGITAL_L2);
+	controls.leftBumper1 = master.get_digital(DIGITAL_L1);
 	controls.rightBumper2 = master.get_digital(DIGITAL_R2);
-	controls.leftBumper2 = master.get_digital(DIGITAL_L2);	
+	controls.leftBumper2 = master.get_digital(DIGITAL_L2);
+	controls.a = master.get_digital_new_press(DIGITAL_A);
 
 	return controls;
 }
 
 // spins grabblers, positive direction --> sucks up
-void grabblerSucc (int grabblerRotation){
-	leftGrabblerMotor.move(grabblerRotation);
-	rightGrabblerMotor.move(-1 * grabblerRotation);
+void grabblerSucc (int grabblerRotation){  // maybe making this double will allow blocks to be sucked out? probs not tho
+	leftGrabblerMotor.move(-1*grabblerRotation);
+	rightGrabblerMotor.move( grabblerRotation);
 }
 
 /**
@@ -85,7 +87,9 @@ void disabled() {
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+void competition_initialize() {
+	autonomous();
+}
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -98,10 +102,40 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+double DistanceToRotations(double inches){
+	double Circumference = 12.5663706144;
+	return inches/Circumference; //why is there a pre-defined variable circumference?
+}
 
+void FlipOutWiggle(){
+	retractRamp.move_absolute(.5,100); //manually see how many rotations it went thru on brain or use degrees
+	for (int i = 0; i<5; i++){
+	frontLeftMotor.move_absolute(DistanceToRotations(3),-100);
+	backLeftMotor.move_absolute(DistanceToRotations(3),-100);
+	frontRightMotor.move_absolute(DistanceToRotations(3),100);
+	backRightMotor.move_absolute(DistanceToRotations(3),100);
 
+	frontLeftMotor.move_absolute(DistanceToRotations(3),100);
+	backLeftMotor.move_absolute(DistanceToRotations(3),100);
+	frontRightMotor.move_absolute(DistanceToRotations(3),-100);
+	backRightMotor.move_absolute(DistanceToRotations(3),-100);
+}
+	retractRamp.move_absolute(.5,100);
+}
 
+void MoveDistance(double distance, double velocity){
+	frontLeftMotor.move_absolute(DistanceToRotations(distance),-velocity); //position then velocity
+	backLeftMotor.move_absolute(DistanceToRotations(distance),-velocity);
+	frontRightMotor.move_absolute(DistanceToRotations(distance),velocity); // for right motors, positive motor spin sends bot back
+	backRightMotor.move_absolute(DistanceToRotations(distance),velocity); ////watch the negative
+}
+
+void autonomous() {
+//FlipOutWiggle();
+MoveDistance(36,100);
+//rightGrabblerMotor.move_absolute(DistanceToRotations(50),-100);
+//grabblerSucc (100); //only going to run once
+}
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -124,30 +158,37 @@ void opcontrol() {
 	while (true) {
 		controls = getControllerState();
 
-		frontLeftMotor.move(controls.left);
-		backLeftMotor.move(controls.left);
+		frontLeftMotor.move(-1*controls.left);
+		backLeftMotor.move(-1*controls.left);
 
-		frontRightMotor.move(-1 * controls.right); // for right motors, positive motor spin sends bot back
-		backRightMotor.move(-1 * controls.right);
+		frontRightMotor.move( controls.right); // for right motors, positive motor spin sends bot back
+		backRightMotor.move( controls.right);
+
+		if (controls.a){
+			autonomous();
+		}
 
 		if (controls.leftBumper1){
-			grabblerSucc (grabblerSpeed); // succ in le blocks
-		} else if (controls.leftBumper2){
-			grabblerSucc (-1 * grabblerSpeed); // succ out le blocks
-		}	else {
+			grabblerSucc (-1*grabblerSpeed); // succ in le blocks
+		}
+	else	if (controls.leftBumper2){
+			grabblerSucc (grabblerSpeed); // succ out le blocks
+		}
+			else {
 			grabblerSucc (0); // stop grabbler movement
 		}
 
 		//move le ramp (chimp)
 		if (controls.rightBumper1 || controls.rightBumper2){
 			if (controls.rightBumper2){ // move tower out
-				retractRamp.move(-1 * grabblerSpeed);
-			} else { // move tower in
 				retractRamp.move(grabblerSpeed);
+			} else { // move tower in
+				retractRamp.move(-1*grabblerSpeed);
 			}
 		} else {
 			retractRamp.move(0);
 		}
+
 
 		delay(tickLength);
 	}
